682f9616203eba1ce839fdc96dbbcb00f58914aa
Yoanvari <yoanvari123@gmail.com>
2025-06-29 03:31:29 +0700

    Add Sidebar component and update routing for Loan page

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
 import Header from "./components/Header.vue";
+import Sidebar from "./components/Sidebar.vue";
 </script>
 
 <template>
-  <div>
-    <Header />
-    <router-view />
+  <div class="flex h-screen font-inter">
+    <Sidebar />
+    <div class="flex-col w-full">
+      <Header />
+      <router-view />
+    </div>
   </div>
 </template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Dashboard from "../pages/dashboard.vue";
+import Dashboard from "../pages/Dashboard.vue";
+import Loan from "../pages/Loan.vue";
 
-const routes = [{ path: "/", component: Dashboard }];
+const routes = [
+  { path: "/", component: Dashboard },
+  { path: "/loan", component: Loan },
+];
 
 const router = createRouter({
   history: createWebHistory(),
