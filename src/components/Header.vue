f519ab9552925c14d0abfe73f92e2949b54ef76b
Yoanvari <yoanvari123@gmail.com>
2025-06-28 22:49:13 +0700

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+  <header class="bg-gray-800 text-white p-4">
+    <nav class="flex gap-4">
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </header>
+</template>
---
+++ src/pages/dashboard.vue
@@ -0,0 +1,5 @@
+<template>
+  <section class="p-4">
+    <h1>Ini halaman Home</h1>
+  </section>
+</template>
