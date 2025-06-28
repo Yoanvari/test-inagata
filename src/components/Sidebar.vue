98121b1522eec52ae6f52ac517bc2ed9c4c989a2
Yoanvari <yoanvari123@gmail.com>
2025-06-29 03:32:14 +0700

    Add Sidebar and SidebarItem components for navigation

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import Icon from "../components/Icon.vue";
+import SidebarItem from "./SidebarItem.vue";
+</script>
+
+<template>
+  <aside class="bg-[#ffffff]">
+    <nav class="flex flex-col text-[#B1B1B1] font-medium text-[18px] w-[250px]">
+      <!-- logo -->
+      <div class="pt-[31px] pl-[38px] pb-[46px]">
+        <Icon name="Logo" class="w-[148px] h-[36px]" />
+      </div>
+      <!-- dashboard -->
+      <SidebarItem to="/dashboard" icon="Home" label="Dashboard" />
+      <!-- transactions -->
+      <SidebarItem to="/transaction" icon="Loan" label="Transactions" />
+      <!-- accounts -->
+      <SidebarItem to="/account" icon="Home" label="Accounts" />
+      <!-- investments -->
+      <SidebarItem to="/investment" icon="Home" label="Investments" />
+      <!-- credit cards -->
+      <SidebarItem to="/credit-card" icon="Home" label="Credit Cards" />
+      <!-- loan -->
+      <SidebarItem to="/loan" icon="Loan" label="Loans" />
+      <!-- services -->
+      <SidebarItem to="/service" icon="Loan" label="Services" />
+      <!-- previlages -->
+      <SidebarItem to="/privilege" icon="Loan" label="My Privileges" />
+      <!-- setting -->
+      <SidebarItem to="/setting" icon="Loan" label="Setting" />
+    </nav>
+  </aside>
+</template>
---
+++ src/components/SidebarItem.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { RouterLink, useRoute } from "vue-router";
+import Icon from "./Icon.vue";
+import { computed } from "vue";
+
+const props = defineProps<{
+  to: string;
+  icon: string;
+  label: string;
+}>();
+
+const route = useRoute();
+
+const isActive = computed(() => route.path === props.to);
+</script>
+
+<template>
+  <div class="w-full flex justify-start relative">
+    <RouterLink
+      :to="to"
+      class="flex flex-row items-center h-[60px] pl-[44px]"
+      :class="isActive ? 'text-[#2D60FF]' : 'text-[#B1B1B1]'"
+    >
+      <Icon :name="icon" class="w-[25px] h-[25px]" />
+      <span class="pl-[26px]">{{ label }}</span>
+    </RouterLink>
+    <div
+      class="absolute bottom-0 left-0 h-full w-[6px] rounded-br-full rounded-tr-full"
+      :class="isActive ? 'bg-[#2D60FF]' : 'bg-white'"
+    ></div>
+  </div>
+</template>
