37b54f5cb042f44611707123826b2cbb7337db00
Yoanvari <yoanvari123@gmail.com>
2025-07-03 23:13:07 +0700

    Add BarChart and ToggleSwitch components for data visualization and user interaction

---
+++ src/components/BarChart.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="h-[322px] rounded-[25px] bg-white py-[28px] pr-[30px] pl-[33px]">
+    <div class="flex flex-row justify-end gap-[30px] pb-[22px]">
+      <span class="flex flex-row gap-[10px] items-center">
+        <div class="size-[15px] rounded-full bg-[#16DBCC]"></div>
+        <div class="text-[#718EBF] text-[15px] font-normal">Deposit</div>
+      </span>
+      <span class="flex flex-row gap-[10px] items-center">
+        <div class="size-[15px] rounded-full bg-[#FF82AC]"></div>
+        <div class="text-[#718EBF] text-[15px] font-normal">Withdraw</div>
+      </span>
+    </div>
+    <section
+      class="relative w-full h-[226px] flex flex-row justify-between gap-[10px]"
+    >
+      <div class="flex flex-col justify-between h-[201px]">
+        <div class="text-[13px] font-normal text-right text-[#718EBF]">500</div>
+        <div class="text-[13px] font-normal text-right text-[#718EBF]">400</div>
+        <div class="text-[13px] font-normal text-right text-[#718EBF]">300</div>
+        <div class="text-[13px] font-normal text-right text-[#718EBF]">200</div>
+        <div class="text-[13px] font-normal text-right text-[#718EBF]">100</div>
+        <div class="text-[13px] font-normal text-right text-[#718EBF]">0</div>
+      </div>
+      <div class="relative z-10 flex flex-row justify-between w-full px-[25px]">
+        <div class="flex flex-col justify-end items-center">
+          <div class="flex flex-row justify-between items-end gap-[12px]">
+            <div class="bg-[#1814F3] h-[178px] w-[15px] rounded-full"></div>
+            <div class="bg-[#16DBCC] h-[91px] w-[15px] rounded-full"></div>
+          </div>
+          <div
+            class="pt-[12px] text-[13px] font-normal text-center text-[#718EBF]"
+          >
+            Sat
+          </div>
+        </div>
+        <div class="flex flex-col justify-end items-center">
+          <div class="flex flex-row justify-between items-end gap-[12px]">
+            <div class="bg-[#1814F3] h-[130px] w-[15px] rounded-full"></div>
+            <div class="bg-[#16DBCC] h-[49px] w-[15px] rounded-full"></div>
+          </div>
+          <div
+            class="pt-[12px] text-[13px] font-normal text-center text-[#718EBF]"
+          >
+            Sun
+          </div>
+        </div>
+        <div class="flex flex-col justify-end items-center">
+          <div class="flex flex-row justify-between items-end gap-[12px]">
+            <div class="bg-[#1814F3] h-[122px] w-[15px] rounded-full"></div>
+            <div class="bg-[#16DBCC] h-[98px] w-[15px] rounded-full"></div>
+          </div>
+          <div
+            class="pt-[12px] text-[13px] font-normal text-center text-[#718EBF]"
+          >
+            Mon
+          </div>
+        </div>
+        <div class="flex flex-col justify-end items-center">
+          <div class="flex flex-row justify-between items-end gap-[12px]">
+            <div class="bg-[#1814F3] h-[178px] w-[15px] rounded-full"></div>
+            <div class="bg-[#16DBCC] h-[138px] w-[15px] rounded-full"></div>
+          </div>
+          <div
+            class="pt-[12px] text-[13px] font-normal text-center text-[#718EBF]"
+          >
+            Tue
+          </div>
+        </div>
+        <div class="flex flex-col justify-end items-center">
+          <div class="flex flex-row justify-between items-end gap-[12px]">
+            <div class="bg-[#1814F3] h-[57px] w-[15px] rounded-full"></div>
+            <div class="bg-[#16DBCC] h-[91px] w-[15px] rounded-full"></div>
+          </div>
+          <div
+            class="pt-[12px] text-[13px] font-normal text-center text-[#718EBF]"
+          >
+            Wed
+          </div>
+        </div>
+        <div class="flex flex-col justify-end items-center">
+          <div class="flex flex-row justify-between items-end gap-[12px]">
+            <div class="bg-[#1814F3] h-[145px] w-[15px] rounded-full"></div>
+            <div class="bg-[#16DBCC] h-[91px] w-[15px] rounded-full"></div>
+          </div>
+          <div
+            class="pt-[12px] text-[13px] font-normal text-center text-[#718EBF]"
+          >
+            Thu
+          </div>
+        </div>
+        <div class="flex flex-col justify-end items-center">
+          <div class="flex flex-row justify-between items-end gap-[12px]">
+            <div class="bg-[#1814F3] h-[147px] w-[15px] rounded-full"></div>
+            <div class="bg-[#16DBCC] h-[126px] w-[15px] rounded-full"></div>
+          </div>
+          <div
+            class="pt-[12px] text-[13px] font-normal text-center text-[#718EBF]"
+          >
+            Fri
+          </div>
+        </div>
+      </div>
+      <div
+        class="absolute z-0 bottom-[30px] left-0 w-full h-[186px] flex flex-col justify-between pl-[36px]"
+      >
+        <div class="w-full border-[1px] border-[#F3F3F5]"></div>
+        <div class="w-full border-[1px] border-[#F3F3F5]"></div>
+        <div class="w-full border-[1px] border-[#F3F3F5]"></div>
+        <div class="w-full border-[1px] border-[#F3F3F5]"></div>
+        <div class="w-full border-[1px] border-[#F3F3F5]"></div>
+        <div class="w-full border-[1px] border-[#F3F3F5]"></div>
+      </div>
+    </section>
+  </div>
+</template>
---
+++ src/components/ToggleSwitch.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const isOn = ref(false);
+</script>
+
+<template>
+  <button
+    @click="isOn = !isOn"
+    :class="[
+      'w-14 h-[31px] rounded-full p-1 flex items-center transition-colors duration-300',
+      isOn ? 'bg-[#16DBCC]' : 'bg-[#DFEAF2]',
+    ]"
+  >
+    <div
+      :class="[
+        'bg-white w-[27px] h-[27px] rounded-full shadow-md transform transition-transform duration-300',
+        isOn ? 'translate-x-[23px]' : 'translate-x-[-2px]',
+      ]"
+    ></div>
+  </button>
+</template>
